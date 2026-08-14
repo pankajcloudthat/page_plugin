c96c72a46929ac820fc08e3146741c22deaee026
Pankaj Choudhary <93825810+pankajcloudthat@users.noreply.github.com>
2026-08-14 20:06:01 +0000

    Add language strings for Training Integration plugin

---
+++ lang/en/local_training.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Training Integration';
+
+$string['local_training_create_page'] = 'Create Moodle Page Activity';
+
+$string['local_training_create_page_description'] =
+    'Creates a Moodle Page activity in a course containing an external media URL.';
